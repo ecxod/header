572e47d87476aac184de873a0ec108261b38b82a
Christian Eichert <c@zp1.net>
2025-04-21 17:31:21 +0200

    modified:   src/header/H.php

--- src/header/H.php
+++ src/header/H.php
@@ -18,16 +18,14 @@ class H extends GLOBALE
     protected $twig;
     protected $twigpath;
 
-    function __construct()
+    public function __construct()
     {
-        $this->cdn_down = false;
+        $this->cdn_down = true;
 
-        $this->twigpath = realpath(__DIR__ . '/../../templates/');
-
-        $this->loader = new FilesystemLoader(paths: $this->twigpath);
+        $this->loader = new FilesystemLoader(paths: $this->getTwigpath() . "/templates/");
 
         // Namespacing : wir erstellen ein twig namespace 'index'
-        $this->loader->addPath(path: $this->twigpath, namespace: 'header');
+        $this->loader->addPath(path: $this->getTwigpath() . "/templates/", namespace: 'header');
 
         // twig Objekt erstellen
         $this->twig = new Environment(loader: $this->loader);
@@ -37,15 +35,15 @@ class H extends GLOBALE
 
     public function head(float $vp = 1.0)
     {
-        return $this->twig->render(name: '@header/_head.twig', context: [
-            'lang' =>  strval(value: explode(separator: '_', string: $_ENV['LANG'])[0]),
+        return $this->twig->render(name: '@header/_head.twig', context: [ 
+            'lang'                     => strval(value: explode(separator: '_', string: $_ENV['LANG'])[0]),
 
-            'charset' => strval(value: $_ENV['CHARSET']),
+            'charset'                  => strval(value: $_ENV['CHARSET']),
 
-            'initialScale' => number_format(num: $vp, decimals: 1),
-            'maximumScale' => number_format(num: $vp, decimals: 1),
- 
-            'resultcash' => $this->cash('no-cache'),
+            'initialScale'             => number_format(num: $vp, decimals: 1),
+            'maximumScale'             => number_format(num: $vp, decimals: 1),
+
+            'resultcash'               => $this->cash('no-cache'),
             // // 'description' => $this->description(),
             // // 'icons' => $this->icons(),
             // // 'apple' => $this->apple(),
@@ -55,10 +53,78 @@ class H extends GLOBALE
             // // 'autor' => $this->autor(),
             // // 'datum' => $this->datum(),
             // // 'canonical' => $this->canonical(),
-            'loadScripts' => $this->loadScripts(),
+            'loadScripts'              => $this->loadScripts(),
 
         ]);
     }
+
+
+    /** Get the Composer Vendor Path
+     * @return string
+     * TODO: sollte irgendwann in ein ecxod/composer umziehen 
+     */
+    protected function getVendorpath(): string
+    {
+        $vendorpath    = "";
+        $envVendorPath = \strval(\realpath($_ENV['VENDOR']));
+        if(!empty($envVendorPath))
+            $vendorpath = $envVendorPath;
+        else
+        {
+            if(empty($_SERVER["DOCUMENT_ROOT"]))
+                die("Please set DOCUMENT_ROOT.");
+            $vendorpath ?? \realpath(path: $_SERVER["DOCUMENT_ROOT"] . "/../vendor/");
+        }
+        if(empty($vendorpath))
+            die("Unable to find Composer Vendor Path!");
+        else
+            return $vendorpath;
+    }
+
+    /** Gets the path of the inner Library Twigg folder
+     *  - it uses getVendorpath() only if no vendorpath was injected in getTwigpath()
+     * TODO: sollte irgendwann in ein ecxod/twigg umziehen 
+     * @param string $vendorPath
+     * @return bool|string
+     */
+    protected function getTwigpath(string $vendorPath = "")
+    {
+        $vendorPath ?? $this->getVendorpath();
+        $namespace = \str_replace(search: '\\', replace: DIRECTORY_SEPARATOR, subject: \strtolower(string: __NAMESPACE__));
+        $twigPath  = \realpath($vendorPath . DIRECTORY_SEPARATOR . $namespace);
+        if(empty($twigPath))
+        {
+            die("The Library " . __NAMESPACE__ . " is missing, damaged or in a wrong version! ERR[H002]");
+        }
+        else
+            return $twigPath;
+    }
+
+    /** Gets the path of the inner Library templates folder
+     *  - it uses getVendorpath() only if no vendorpath was injected in getTwigpath()
+     * TODO: sollte irgendwann in ein ecxod/twigg umziehen 
+     * @param string $templatePath
+     * @param string $vendorPath
+     * @return bool|string
+     */
+    protected function getTwigTemplatespath(string $templatePath = "templates", string $vendorPath = ""): bool|string
+    {
+        $vendorPath ?? $this->getVendorpath();
+        $twigTemplatePath = \realpath($this->getTwigpath($vendorPath) . DIRECTORY_SEPARATOR . $templatePath);
+        if(empty($twigTemplatePath))
+        {
+            die("The Library " . __NAMESPACE__ . " is missing, damaged or in a wrong version! ERR[H003]");
+        }
+        else
+            return $twigTemplatePath;
+    }
+
+
+
+
+
+
+
 
 
 
@@ -66,22 +132,26 @@ class H extends GLOBALE
      * @param string|int $cash 
      * @return string 
      */
-    function cash(string|int $cash): string
+    public function cash(string|int $cash): string
     {
         $txt = m(m: __METHOD__);
 
         // todo . nachdenken ob cache sinn macht in diesem fall
-        if ($cash === "no-cache") {
+        if($cash === "no-cache")
+        {
             $txt .=
                 '<meta http-equiv="expires" content="0">' . PHP_EOL .
                 '<meta http-equiv="CACHE-CONTROL" content="NO-CACHE">' . PHP_EOL;
-        } else
-            if (!empty(intval($cash))) {
-            if (intval($cash) < 3600) {
-                $cash = 3600;
-            }
-            $txt .= '<meta http-equiv="Cache-Control" content="max-age=' . strval($cash) . ', must-revalidate">' . PHP_EOL;
         }
+        else
+            if(!empty(intval($cash)))
+            {
+                if(intval($cash) < 3600)
+                {
+                    $cash = 3600;
+                }
+                $txt .= '<meta http-equiv="Cache-Control" content="max-age=' . strval($cash) . ', must-revalidate">' . PHP_EOL;
+            }
 
         $txt .=
             '<meta name="generator" content="MediaWiki 1.34.0-wmf.23">' . PHP_EOL .
@@ -98,11 +168,12 @@ class H extends GLOBALE
     /** Loading Scripts from CDN ( jquery, bootstrap js)
      * @return string
      */
-    function loadScripts(): string
+    public function loadScripts(): string
     {
         $txt = m(m: __METHOD__);
 
-        if (isset($_ENV['CDN_JQ']) and $this->ping($_ENV['CDN_JQ']) !== "down") {
+        if(isset($_ENV['CDN_JQ']) and $this->ping($_ENV['CDN_JQ']) !== "down")
+        {
             // defer = laden wenn alles rum ist
             $txt .= '<script type="text/javascript" src="' . $_ENV['CDN_JQ'] . '"></script>' . PHP_EOL;
             //$txt .= '<script type="text/javascript" src="' . $_ENV['CDN_PO'] . '"></script>' . PHP_EOL;
@@ -110,7 +181,9 @@ class H extends GLOBALE
             $txt .= '<script type="text/javascript" src="' . $_ENV['CDN_JS_BUNDLE'] . '"></script>' . PHP_EOL;
 
             //$txt .= '<script type="text/javascript" src="' . $_ENV['PRISM_JS'] . '"></script>' . PHP_EOL;
-        } else {
+        }
+        else
+        {
             $txt .= '<script type="text/javascript" src="/static/@jquery/dist/jquery.min.js"></script>' . PHP_EOL;
             // popper muss vor bootstrapjs, oder man muss bootsrap.bundle nutzen
             //$txt .= '<script type="text/javascript" src="/static/@popperjs/core/dist/umd/popper.min.js"></script>' . PHP_EOL;
@@ -125,28 +198,34 @@ class H extends GLOBALE
     /** Ladet die Bootstrap css und die icons
      * @return string
      */
-    function loadBootstrapCssAndIcons()
+    public function loadBootstrapCssAndIcons()
     {
         // $txt = m(__METHOD__);
         $txt = "";
         //CDN_BI
-        if (isset($_ENV['CDN_BI']) and $this->ping($_ENV['CDN_BI']) !== "down") {
+        if(isset($_ENV['CDN_BI']) and $this->ping($_ENV['CDN_BI']) !== "down")
+        {
             $txt .= '<link rel="stylesheet" type="text/css" href="' . $_ENV['CDN_BI'] . '" as="style" Cache-Control="max-age=3600">' . PHP_EOL;
             $txt .= '<link rel="preload" type="text/css" href="' . $_ENV['CDN_BI'] . '" as="style">' . PHP_EOL;
             $txt .= '<link rel="prefetch" href="' . $_ENV['CDN_BI'] . '">' . PHP_EOL;
-        } else {
+        }
+        else
+        {
             // CDN_CSS VON LOKAL /usr/lib/composer/vendor/twbs/bootstrap/dist => bs/dist
             $txt .= '<link rel="stylesheet" type="text/css" href="/static/bs/font/bootstrap-icons.min.css" as="style" Cache-Control="max-age=3600">' . PHP_EOL;
             $txt .= '<link rel="preload" type="text/css" href="/static/bs/font/bootstrap-icons.min.css" as="style">' . PHP_EOL;
             $txt .= '<link rel="prefetch" href="/static/bs/font/bootstrap-icons.min.css">' . PHP_EOL;
         }
         //CDN_CSS VON OBSI
-        if (isset($_ENV['CDN_CSS']) and $this->ping($_ENV['CDN_CSS']) !== "down") {
+        if(isset($_ENV['CDN_CSS']) and $this->ping($_ENV['CDN_CSS']) !== "down")
+        {
 
             $txt .= '<link rel="stylesheet" type="text/css" href="' . $_ENV['CDN_CSS'] . '" as="style" Cache-Control="max-age=3600">' . PHP_EOL;
             $txt .= '<link rel="preload" type="text/css" href="' . $_ENV['CDN_CSS'] . '" as="style">' . PHP_EOL;
             $txt .= '<link rel="prefetch" href="' . $_ENV['CDN_CSS'] . '">' . PHP_EOL;
-        } else {
+        }
+        else
+        {
             // CDN_CSS VON LOKAL /usr/lib/composer/vendor/twbs/bootstrap/dist => bs/dist
             $txt .= '<link rel="stylesheet" type="text/css" href="/static/bs/dist/css/bootstrap.min.css" as="style" Cache-Control="max-age=3600">' . PHP_EOL;
             $txt .= '<link rel="preload" type="text/css" href="/static/bs/dist/css/bootstrap.min.css" as="style">' . PHP_EOL;
@@ -161,18 +240,21 @@ class H extends GLOBALE
         return $txt;
     }
 
-    function ping($host, $port = 80, $timeout = 10)
+    public function ping($host, $port = 80, $timeout = 10)
     {
         $starttime = microtime(true);
-        $file = @fsockopen($host, $port, $errno, $errstr, $timeout);
-        $stoptime = microtime(true);
+        $file      = @fsockopen($host, $port, $errno, $errstr, $timeout);
+        $stoptime  = microtime(true);
 
-        if (!$file) {
+        if(!$file)
+        {
             $this->cdn_down = true;
             return "down";
-        } else {
+        }
+        else
+        {
             fclose($file);
-            $status = ($stoptime - $starttime) * 1000;
+            $status         = ($stoptime - $starttime) * 1000;
             $this->cdn_down = false;
             return round($status, 0) . " ms";
         }
@@ -183,41 +265,52 @@ class H extends GLOBALE
      *  SetEnv CSS          /buchungssatz/static/css/
      *  SetEnv JS           /buchungssatz/static/js/
      */
-    function loadStyles()
+    public function loadStyles()
     {
         $txt = m(__METHOD__);
-        $ds = $_ENV['HTTP'] . DIRECTORY_SEPARATOR .  $_ENV['CSS'];
+        $ds  = $_ENV['HTTP'] . DIRECTORY_SEPARATOR . $_ENV['CSS'];
 
-        if (isMobile()) {
+        if(isMobile())
+        {
             $files = array(
                 'menu-mobile.min.css',
             );
 
             // CSS wenn Mobile
-            foreach ($files as $file) {
-                if (file_exists($_ENV['CSS_ROOT'] . $file)) {
+            foreach($files as $file)
+            {
+                if(file_exists($_ENV['CSS_ROOT'] . $file))
+                {
                     $txt .= '<link rel="stylesheet" type="text/css" href="' . $ds . $file . '" as="style" media="(max-width: 480px)" Cache-Control="max-age=36000">' . PHP_EOL;
                     $txt .= '<link rel="preload" type="text/css" href="' . $ds . $file . '" as="style">' . PHP_EOL;
                     $txt .= '<link rel="prefetch" href="' . $ds . $file . '" >' . PHP_EOL;
-                } else {
+                }
+                else
+                {
                     //F::logg("$file does not exist?", __METHOD__, __LINE__);
                 }
             }
-        } else {
+        }
+        else
+        {
             $files = array(
                 'menu.min.css',
                 'buchungssatz.min.css',
                 'switch.min.css',
                 'main0.min.css',
-                'datalist.min.css'
+                'datalist.min.css',
             );
             // CSS when not mobile
-            foreach ($files as $file) {
-                if (file_exists($_ENV['CSS_ROOT'] . $file)) {
+            foreach($files as $file)
+            {
+                if(file_exists($_ENV['CSS_ROOT'] . $file))
+                {
                     $txt .= '<link rel="stylesheet" type="text/css" href="' . $ds . $file . '" media="screen" as="style" Cache-Control="max-age=36000">' . PHP_EOL;
                     $txt .= '<link rel="preload" type="text/css" href="' . $ds . $file . '" as="style">' . PHP_EOL;
                     $txt .= '<link rel="prefetch" href="' . $ds . $file . '" >' . PHP_EOL;
-                } else {
+                }
+                else
+                {
                     //F::logg("$file does not exist?", __METHOD__, __LINE__);
                 }
             }
@@ -231,7 +324,7 @@ class H extends GLOBALE
     }
 
 
-    function scripteNachladen()
+    public function scripteNachladen()
     {
         $txt = m(__METHOD__);
         $txt .= $this->javamuell();
@@ -249,7 +342,7 @@ class H extends GLOBALE
      * @param mixed $rhm
      * @return string
      */
-    function javamuell()
+    public function javamuell()
     {
         $txt = m(__METHOD__);
         $txt .= h::loadScriptSRC(src: 'copyToClipboard_.min.js');
@@ -265,16 +358,18 @@ class H extends GLOBALE
      * @param string|null $uri
      * @return string
      */
-    static function loadScriptSRC(string $src = null, string $uri = null)
+    public static function loadScriptSRC(string $src = null, string $uri = null)
     {
         $txt = m(__METHOD__);
 
-        if (empty($uri) and !empty($src)) {
+        if(empty($uri) and !empty($src))
+        {
             // $uri = $_ENV['HTTP'] . DIRECTORY_SEPARATOR . $_ENV['JS'] . $src;
             $srcuri = DIRECTORY_SEPARATOR . $_ENV['JS'] . $src;
         }
 
-        if (!empty($srcuri)) {
+        if(!empty($srcuri))
+        {
             $txt .= '<script type="text/javascript" charset="' . $_ENV['CHARSET'] . '" src="' . $srcuri . '"></script>' . PHP_EOL;
         }
         return $txt;
